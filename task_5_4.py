8b733bf94f96b4c955089037ccf6ca2467b0d2cb
Dmitry <brothersds@ngs.ru>
2021-01-16 23:49:09 +0300

    completed task_4

--- task_5_4.py
+++ task_5_4.py
@@ -8,3 +8,10 @@ Four — 4
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл.
 '''
+
+my_dict = {'One': 'Один', 'Two': 'Два', 'Three': 'Три', 'Four': 'Четыре'}
+with open(r'text_task_4_2.txt', 'w', encoding='utf-8') as f_obj_temp:
+    with open(r'text_task_4.txt', encoding='utf-8') as f_obj:
+        f_obj.seek(0)
+        for line in f_obj:
+            f_obj_temp.write(''.join([my_dict[line.split()[0]], line.strip()[-4:], '\n']))
