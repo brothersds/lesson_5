c4f2b5398df74461f7edd362fbbf04a7c6809e45
Dmitry <brothersds@ngs.ru>
2021-01-17 02:26:57 +0300

    completed task_7

--- task_5_7.py
+++ task_5_7.py
@@ -12,3 +12,23 @@
 [{"firm_1": 5000, "firm_2": 3000, "firm_3": 1000}, {"average_profit": 2000}]
 Подсказка: использовать менеджер контекста.
 '''
+
+import json
+
+with open(r'text_task_7.txt', encoding='utf-8') as f_obj:
+    f_obj.seek(0)
+    count, average_profit = 0, 0
+    my_dict, avg_profit_dict = {}, {}
+    for element in f_obj:
+        company_profit = float(element.split()[2]) - float(element.split()[3])
+        if company_profit > 0:
+            count += 1
+            average_profit += company_profit
+        my_dict[element.split()[0]] = company_profit
+    avg_profit_dict['average_profit'] = round(average_profit / count, 2)
+    print(f'{[my_dict, avg_profit_dict]}')
+with open(r'text_task_7.json', 'w+', encoding='utf-8') as f_obj:
+    json.dump([my_dict, avg_profit_dict], f_obj)
+with open(r'text_task_7.json', encoding='utf-8') as f_obj:
+    data = json.load(f_obj)
+    print(data)
